1053e3b9d9ae16856aa056bd9403061e28847075
sokkis <sokkis@gmail.com>
2011-12-14 18:09:06 +0000

    Now compatible with the newest Facebook sdk ( included )

--- example.php
+++ example.php
@@ -8,30 +8,33 @@
 <?php
 //////////////////////////////////////////////////////////////
     function autoload($class)
-    {    
+    {
         if(file_exists("facebook/{$class}.php"))
         {
-            @include_once("facebook/{$class}.php");            
-        }    
+            @include_once("facebook/{$class}.php");
+        }
     }
 
     spl_autoload_register("autoload");
 //////////////////////////////////////////////////////////////
 
-    // Create permissions...        
+    // Create permissions...
     $perms = new FacebookPermissions;
     $perms->user_birthday = true;
-    
-    // Get facebook api instance    
+    // Get facebook api instance
+
     $api = FacebookApi::getInstance($perms);
-    
-    // If need additional perms, change them    
+
+    // If need additional perms, change them
     $perms->user_about_me = true;
+
     $api->changePerms($perms);
-    
+
     print_r($api->user); // The current user
 
-/*  Uncomment to create post on your wall
+/*
+//  Uncomment to create post on your wall
+
     $f = new FacebookPost;
     $f->message  = "Teszt Elek";
     $f->caption = "Teszt";
@@ -39,21 +42,21 @@
     $f->name = 'test';
     $f->link = "http://google.com";
     $f->actions[] = array("name"=> "View on Google", "link"=> "http://www.google.com");
-    $f->publish();
+    echo $f->publish();
+    echo $f->lasterror;
     echo $f->id;    // The id of the new post
 */
 
-
 /*
-    Uncomment to create test event...
+
+//    Uncomment to create test event...
     $event = new FacebookEvent; // create empty event
     $event->name="Test";        // Name
     $event->start_time = date("Y-m-20"); // Start
-    $event->end_time = date("Y-m-25");  // End 
+    $event->end_time = date("Y-m-25");  // End
     $event->description = "Description of the event";   //Descr
     $event->publish();      // Publish event
 */
-
 
 ?>
 </pre>
